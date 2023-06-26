4b0b75a4fda38f9d95067a2eda6af66d17590bcb
昏晨 <hh18779682718@163.com>
2023-06-26 09:38:29 +0800

    添加接口自动生成API

---
+++ commons/commons-apis/src/main/java/com/qf/wisdomTree/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.qf.wisdomTree.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包下controller生成API文档
+                //               .apis(RequestHandlerSelectors.basePackage("com.qf.controller"))
+                //为有@Api注解的Controller生成API文档
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SwaggerUI演示")
+                .description("qianfeng")
+                .contact(new Contact("昏晨","西部硅谷","2198919944@qq.com"))
+                .version("1.0")
+                .build();
+    }
+
+
+}
